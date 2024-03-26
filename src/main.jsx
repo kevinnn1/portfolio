7991a952fff77c37439ca03d9b9904634f716bc6
kevinnn1 <47250733+kevinnn1@users.noreply.github.com>
2024-03-26 14:38:35 -0400

    updated resume

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { HashRouter } from "react-router-dom"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter >
+ 
         <App />
     </HashRouter>
 </React.StrictMode>
